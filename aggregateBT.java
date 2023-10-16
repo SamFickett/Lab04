69869c549c6c809b0d24647fec33642469c4ebce
Abigail Pitcairn <abigail.pitcairn@maine.edu>
2023-10-16 15:00:26 -0400

    testing and comments
    added testing, updated comments

--- aggregateBT.java
+++ aggregateBT.java
@@ -1,14 +1,18 @@
 /**
  * @author abigail pitcairn
- * Subtask 3: Create class aggregateBT that extends BrinaryTree class. Inside this class, implement
- * two recursive methods:
- * 1. public int countEvenNumber(): returns the number of nodes with even values
- * 2. public int findSum(): returns the summation of values in the tree
- * Override the testing method, provide a sample tree, and call the two methods above in this
- * method and print the output
+ * @version 16 october 2023
+ * 
+ * implements public int countEvenNumber(): returns the number of nodes with even values
+ * implements public int findSum(): returns the summation of values in the tree
+ * overrides the testing method, provides a sample tree, and calls the two methods above 
+ * and prints the output
  */
 public class aggregateBT extends BinaryTree {
 
+    /**
+     * count the number of even numbers in the BinaryTree
+     * @return number of even numbers
+     */
     public int countEvenNumber(){
         Node localRoot = root;
         if (localRoot == null)
@@ -18,12 +22,34 @@ public class aggregateBT extends BinaryTree {
         return countEvenNumer(localRoot.right) + countEvenNumber(localRoot.left);
     }
 
+    /**
+     * sum all the data in the BinaryTree
+     * @return the sum
+     */
     public int findSum(){
         Node localRoot = root;
         if (localRoot == null)
             return 0;
         return findSum(localRoot.right.data) + findSum(localRoot.left.data);
     }
-    
+
+    /**
+     * testing for countEvenNumber and findSum
+     */
+    @Override
+    public void testing(){
+        BinaryTree testTree = new BinaryTree();
+        Node node = new Node;
+        testTree.root = node;
+        root.data = 4;
+        root.left = new Node;
+        root.left.data = 5;
+        root.right = new Node;
+        root.right.data = 6;
+        System.out.println("Testing Count Even Number: ");
+        System.out.println("Expected: 2    Returned: " + countEvenNumber());
+        System.out.println("Testing Find Sum: ");
+        System.out.println("Expected: 15    Returned: " + findSum());
+    }
 
 }
