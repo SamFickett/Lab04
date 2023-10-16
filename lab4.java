499628eb9cb4a4af35e14868d2389850140ac13c
SamFickett <144719689+SamFickett@users.noreply.github.com>
2023-10-16 14:49:22 -0400

    Create lab4.java

---
+++ lab4.java
@@ -0,0 +1,12 @@
+public class lab4 {
+  public static void main(String args[]) {
+    aggregateBT ABT = new aggregateBT();
+    ABT.test();
+
+    searchBT SBT = nw searchBT();
+    SBT.test();
+
+    BTPrinting BTP = newBTPrinting();
+    BTP.test();
+  }
+}
