9eedb75d465dbca50f126251c1859ebba914b7bf
SamFickett <144719689+SamFickett@users.noreply.github.com>
2023-10-16 14:37:31 -0400

    Update BinaryTree.java
    changing

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,30 +1,15 @@
-import org.w3c.dom.Node;
-
-public class BinaryTree<Integer> {
-	int data = 0;
+public class BinaryTree {
+	
+	protected class Node {
+		int data;
+		Node left;
+		Node right;
+	}
+	
 	protected Node root;
 	
-	public void testPostOrder() {
-		root = postOrder();
+	public void test() {
+		
 	}
 	
-	public void testPrintRight() {
-		root = printRight();
-	}
-	
-	public void testGreater(int item) {
-		root = containsGreater(item);
-	}
-	
-	public void testGetLevel(int item) {
-		root = getLevel(item);
-	}
-	
-	public void testEvenNumber() {
-		root = countEvenNumber();
-	}
-	
-	public void testFindSum() {
-		root = findSum();
-	}
 }
