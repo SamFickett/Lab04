9b2755d5957b60150ccf2d56a19d43850a1c826e
Abdul13002 <133472765+Abdul13002@users.noreply.github.com>
2023-10-16 17:43:54 -0400

    Update searchBT.java

--- searchBT.java
+++ searchBT.java
@@ -1,46 +1,51 @@
-/**
- * 
- */
 
-/**
- * @author abduladdan
- *
- */
+
+// Abdul Addan Subtask 2
 public class searchBT extends BinaryTree {
 
-	public searchBT(int item) {
-		super();
+    // Method to check if there is an item greater than the provided item in the tree
+    public boolean containsGreater(int item) {
+        return containsGreater(root, item);
+    }
 
-	}
+    private boolean containsGreater(Node node, int item) {
+        // Base case
+        if (node == null) {
+            return false;
+        }
 
-	public boolean containsGreater(int item) {
-		return this.containsGreaterRecursive(this, item);
+        // Check if the current node's data is greater than the item
+        if (node.data > item) {
+            return true;
+        }
 
-	}
+        // Recursively check the left and right subtrees
+        return containsGreater(node.left, item) || containsGreater(node.right, item);
+    }
 
-	public boolean containsGreaterRecursive(BinaryTree node, int item) {
-		if (node == null)
-			return false;
-		else {
-			return true;
-		}
+    // Method to find the level of a given item in the tree
+    public int getLevel(int item) {
+        return getLevel(root, item, 1);
+    }
 
-	}
+    private int getLevel(Node node, int item, int level) {
+        // Base case
+        if (node == null) {
+            return -1;
+        }
 
-	public int getLevel(int item) {
-		return this.getLevelRecursive(this, item);
+        // If the item is found, return the level
+        if (node.data == item) {
+            return level;
+        }
 
-	}
+        // If the item is present in the left subtree, return its level
+        int leftLevel = getLevel(node.left, item, level + 1);
+        if (leftLevel != -1) {
+            return leftLevel;
+        }
 
-	private int getLevelRecursive(BinaryTree node, int item) {
-		if (node == null)
-			return -1;
-		
-  } {	if (node.data == null)
-			return level;
-
-		{
-
-		}
-	}
+        // If the item is present in the right subtree, return its level
+        return getLevel(node.right, item, level + 1);
+    }
 }
